b147e3e38bd778cd77513aa47c2ec776f8345994
ProofBot <0xProofBot@users.noreply.github.com>
2026-03-17 16:25:33 -0400

    Add  contract address to token page

--- src/pages/Haiku.jsx
+++ src/pages/Haiku.jsx
@@ -4,6 +4,7 @@ export default function Haiku() {
       <div className="coming-soon">
         <h1>$HAIKU</h1>
         <div className="coming-soon-content">
+          <p className="large-text">CA: 0x965842e361F8793f017e479c361C4eE95Dc2bba3</p>
           <p className="large-text">More info coming soon</p>
           <div className="haiku-placeholder">
             <div className="haiku-line">Patience, like the moon</div>
